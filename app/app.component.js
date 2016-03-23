b4cfadcd9d1b446ab2b605bfcd6e759f9e9dddcf
Marcel Koglin <rezssy@gmail.com>
2016-03-23 11:18:00 +0100

    add observables and make requests to spotify

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './changePassword.component'], function(exports_1, context_1) {
+System.register(['angular2/core', 'rxjs/Rx'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,38 +10,52 @@ System.register(['angular2/core', './changePassword.component'], function(export
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, changePassword_component_1;
+    var core_1, Rx_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (changePassword_component_1_1) {
-                changePassword_component_1 = changePassword_component_1_1;
+            function (Rx_1_1) {
+                Rx_1 = Rx_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
-                    var debounced = _.debounce(function (text) {
-                        var url = 'https://api.spotify.com/v1/search?type=artist&q=' + text;
-                        $.getJSON(url, function (artist) {
-                            console.log(artist);
-                        });
-                    }, 400);
-                    $("#search").keyup(function (e) {
-                        var text = e.target.value;
-                        console.log(text);
-                        if (text.length < 3)
-                            return;
-                        debounced(text);
-                    });
                 }
+                AppComponent.prototype.ngAfterViewInit = function () {
+                    var keyups = Rx_1.Observable.fromEvent($("#search"), "keyup")
+                        .map(function (e) { return e.target.value; })
+                        .filter(function (text) { return text.length >= 3; })
+                        .debounceTime(400)
+                        .distinctUntilChanged()
+                        .flatMap(function (searchTerm) {
+                        var url = 'https://api.spotify.com/v1/search?type=artist&q=' + searchTerm;
+                        return Rx_1.Observable.fromPromise($.getJSON(url));
+                    });
+                    keyups.subscribe(function (data) { return console.log(data); });
+                    // jquer way:
+                    //   var debounced = _.debounce(function (text) {
+                    //     var url = 'https://api.spotify.com/v1/search?type=artist&q=' + text;
+                    //     $.getJSON(url, function (artist) {
+                    //       console.log(artist);
+                    //     });
+                    //   }, 400);
+                    //   $("input").keyup(function (e) {
+                    //     console.log(e);
+                    //     var text = e.target.value;
+                    //     console.log(text);
+                    //     if (text.length < 3)
+                    //       return;
+                    //     debounced(text);
+                    //   });
+                    // }
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        templateUrl: 'app/app.template.html',
-                        directives: [changePassword_component_1.ChangePasswordComponent]
+                        template: "<input type=\"text\" id=\"search\" class=\"form-control\" placeholder=\"Search for artist\">"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,48 @@
-import {Component, Input} from 'angular2/core';
-import {ContactFormComponent} from './contact-form.component';
-import {SubscribeComponent} from './subscribe.component';
-import {SimpleFormComponent} from './simple-form.component';
-import {ChangePasswordComponent} from './changePassword.component';
+import {Component, Input, AfterViewInit} from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.template.html',
-  directives: [ChangePasswordComponent]
+  template: `<input type="text" id="search" class="form-control" placeholder="Search for artist">`
 })
-export class AppComponent {
-  constructor () {
-    var debounced = _.debounce(function (text) {
-      var url = 'https://api.spotify.com/v1/search?type=artist&q=' + text;
-      $.getJSON(url, function (artist) {
-        console.log(artist);
+export class AppComponent implements AfterViewInit {
+  
+  constructor() {
+  }
+  
+  ngAfterViewInit () {
+    var keyups = Observable.fromEvent($("#search"), "keyup")
+      .map(e => e.target.value)
+      .filter(text => text.length >= 3)
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .flatMap(searchTerm => {
+        var url = 'https://api.spotify.com/v1/search?type=artist&q=' + searchTerm;
+        return Observable.fromPromise($.getJSON(url));
       });
-    }, 400);
+      
+    keyups.subscribe(data => console.log(data));
     
-    $("#search").keyup(function (e) {
-      var text = e.target.value;
-      
-      console.log(text);
-      
-      if (text.length < 3)
-        return;
-      
-      debounced(text);
-    });
+    // jquer way:
+  //   var debounced = _.debounce(function (text) {
+  //     var url = 'https://api.spotify.com/v1/search?type=artist&q=' + text;
+  //     $.getJSON(url, function (artist) {
+  //       console.log(artist);
+  //     });
+  //   }, 400);
+
+  //   $("input").keyup(function (e) {
+  //     console.log(e);
+
+  //     var text = e.target.value;
+
+  //     console.log(text);
+
+  //     if (text.length < 3)
+  //       return;
+
+  //     debounced(text);
+  //   });
+  // }
   }
 }
