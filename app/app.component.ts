2a8ea21dbb132ee90b16f12c49b9dbd332731dfb
Marcel Koglin <rezssy@gmail.com>
2016-03-24 12:18:00 +0100

    add retry if ajax call fails

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,11 +6,13 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/retry';
 import 'rxjs/add/observable/fromArray';
 import 'rxjs/add/observable/range';
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/throw';
 
 @Component({
   selector: 'my-app',
@@ -44,16 +46,16 @@ export class AppComponent implements AfterViewInit {
     // Observable.fromArray(startDates).map(date => { console.log("Getting deals from for date " + date); return [1, 2, 3]; }).subscribe(x => console.log(x));
     // Observable.interval(1000).flatMap(x => { console.log('calling the server to get the latest news'); return Observable.of([1, 2, 3]); }).subscribe(x => console.log(x));
     
-    var userStream = Observable.of({ userId: 1, username: 'mosh' }).delay(2000);
-    var tweetsStream = Observable.of([1, 2, 3]).delay(1500);
+    // Observable.throw(new Error('Something failed.')).subscribe(x => console.log(x), error => console.error(error));
+    var counter = 0;
+    var ajaxCall = Observable.of('url').delay(1500).flatMap(() => {
+      if (++counter < 2)
+        return Observable.throw(new Error('request failed.'));
+        
+      return Observable.of([1, 2, 3]);
+    });
     
-    Observable.forkJoin(userStream, tweetsStream)
-      .map(input => 
-        // [{ userId: 1, username: 'mosh'}, [1, 2, 3]]
-        ({ 'id': input[0]['userId'], 'name': input[0]['username'], 'tweet': input[1] })
-        // new Object({user: input[0], tweets: input[1]})
-      )
-      .subscribe(result => console.log(result));
+    ajaxCall.retry(3).subscribe(x => console.log(x), error => console.error(error));
     
   }
 
